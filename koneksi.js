81df8b54ad6fb810b253b607a3842c6b4f33703f
Didi Ruswandi <didi.ruswandi@admedika.co.id>
2022-12-28 12:57:22 +0700

    tambah res

--- koneksi.js
+++ koneksi.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 //buat koneksi database
 const conn = mysql.createConnection({
-    host:'localhost',
+    host:'localhost:90',
     user:'root',
     password:'',
     database:'dbrestapi'
